e738db21d522fc29013833e7ab10f4b24860d583
KozakaiAya <zju3150105246@outlook.com>
2019-09-27 00:00:55 -0400

    proper implementation of get_folderlist_by_depth consider relative depth

--- utils/folder_ops.py
+++ utils/folder_ops.py
@@ -12,11 +12,37 @@ def get_size(path):
     elif os.path.isfile(path):
         return os.path.getsize(path)
 
+def within_subdir(path, start, proper=False):
+    rel_path = os.path.relpath(path, start)
+    if rel_path.startswith('..'):
+        return False
+    elif rel_path == '.':
+        # proper=True, same path is not proper subdir
+        # proper=False, same path is subdir
+        return not proper
+    else:
+        return True
+
+def is_samepath(path1, path2):
+    path1_norm = os.path.normpath(path1)
+    path2_norm = os.path.normpath(path2)
+    return path1_norm == path2_norm
+
+def get_rel_depth(path, start):
+    if within_subdir(path, start, proper=True):
+        rel_path = os.path.relpath(path, start)
+        depth = rel_path.count(os.path.sep) + 1
+        return depth
+    elif is_samepath(path, start):
+        return 0
+    else:
+        return -1 
+
 def get_folderlist_by_depth(path, depth):
     path = os.path.normpath(path)
     res = []
     for root, dirs, files in os.walk(path, topdown=True):
-        cur_depth = root[len(os.path.sep):].count(os.path.sep) + 1
+        cur_depth = get_rel_depth(root, path) + 1
         if cur_depth == depth:
             res += [os.path.join(root, d) for d in dirs]
             res += [os.path.join(root, f) for f in files]
