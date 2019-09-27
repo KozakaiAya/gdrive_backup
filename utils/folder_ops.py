5cb4ac68aaf355966be45278f8f41f61844ef09f
KozakaiAya <zju3150105246@outlook.com>
2019-09-26 23:04:19 -0400

    fix get_size to accept file and directory

--- utils/folder_ops.py
+++ utils/folder_ops.py
@@ -1,12 +1,16 @@
 import os
 
-def get_size(start_path):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
+def get_size(path):
+    if os.path.isdir(path):
+        # the start_path is a regular folder
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return total_size
+    elif os.path.isfile(path):
+        return os.path.getsize(path)
 
 
 def level_walk(some_dir, level=1):
